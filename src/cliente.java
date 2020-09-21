1c4d2c59b36641906d9917ac9b04eb0b707cc242
Felipe_Vargas13 <pipevarji@gmail.copm>
2020-09-20 23:29:00 -0600

    Class cliente and Server finish, First part

--- src/cliente.java
+++ src/cliente.java
@@ -13,3 +13,47 @@ public class Cliente {
     }
 
 }
+
+class MarcoCliente extends JFrame{
+
+    public MarcoCliente(){
+
+        setBounds(600,300,280,350);
+
+        LaminaMarcoCliente milamina=new LaminaMarcoCliente();
+
+        add(milamina);
+
+        setVisible(true);
+    }
+
+}
+
+class LaminaMarcoCliente extends JPanel{
+
+    public LaminaMarcoCliente(){
+
+        JLabel texto=new JLabel("CLIENTE");
+
+        add(texto);
+
+        campo1=new JTextField(20);
+
+        add(campo1);
+
+        miboton=new JButton("Enviar");
+
+        add(miboton);
+
+    }
+
+
+
+
+
+
+    private JTextField campo1;
+
+    private JButton miboton;
+
+}
