d295943f108fa017cc106f5a65d8eb3d7d821a11
Felipe_Vargas13 <pipevarji@gmail.copm>
2020-09-20 21:13:27 -0600

    commit incial

---
+++ src/cliente.java
@@ -0,0 +1,20 @@
+
+package soskets1;
+
+public class cliente {
+    public static void main(Streign[],args){
+
+     ServerSocket = null;
+     Socket sc = null;
+     final int PUERTO 5000;
+
+
+     try{
+         servidor = new ServerSocket (PUERTO)
+
+
+     } catch (IOException ex){
+          Lo
+     }
+    }
+}
